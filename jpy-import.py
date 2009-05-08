29ada8d3dbef7fd4b1db3f19cfd34ab7cf9aa502
Benoît Ryder <benoit@ryder.fr>
2009-05-08 17:21:34 +0000

    Initial commit

---
+++ jpy-import.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+# vim: fileencoding=utf-8
+
+import xml.sax
+import sqlite3
+import re, sys
+
+
+# Last JMdict version (English only)
+jmdict_url = 'http://ftp.monash.edu.au/pub/nihongo/JMdict_e.gz'
+
+
+class JMDictHandler(xml.sax.handler.ContentHandler):
+
+  def __init__(self, db):
+    self.db = db
+
+  # Collect entity declarations, to back-resolve entities
+  def EntityDeclHandler(self, entityName, is_parameter_entity, value, base, systemId, publicId, notationName):
+    self.ent[value] = entityName
+
+  def startDocument(self):
+    self._locator._ref._parser.EntityDeclHandler = self.EntityDeclHandler
+    self.ent = {}
+
+    self.conn = sqlite3.connect(self.db)
+    cursor = self.conn.cursor()
+
+    for s in ('kanji', 'reading', 'sense', 'gloss'):
+      cursor.execute("DROP TABLE IF EXISTS %s"%s)
+
+    cursor.execute("""
+    CREATE TABLE kanji (
+      ent_id INT NOT NULL,
+      keb TINYTEXT NOT NULL
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE reading (
+      ent_id INT NOT NULL,
+      reb TINYTEXT NOT NULL,
+      romaji TINYTEXT NOT NULL
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE sense (
+      ent_id INT NOT NULL,
+      sense_num INT NOT NULL,
+      pos VARCHAR(50) NOT NULL,
+      attr VARCHAR(50) NOT NULL,
+      PRIMARY KEY (ent_id, sense_num)
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE gloss (
+      ent_id INT NOT NULL,
+      sense_num INT NOT NULL,
+      lang VARCHAR(5) NOT NULL,
+      gloss TEXT NOT NULL
+    )
+    """)
+
+    cursor.execute("CREATE INDEX k_ent ON kanji (ent_id)")
+    cursor.execute("CREATE INDEX r_ent ON reading (ent_id)")
+    #cursor.execute("CREATE INDEX r_reb ON reading (reb)")
+    cursor.execute("CREATE INDEX g_sense ON gloss (ent_id, sense_num)")
+    #cursor.execute("CREATE INDEX g_gloss ON gloss (gloss)")
+
+    self.conn.commit()
+
+    self.cur_entry = None
+    self.cur_sense = None
+    self.txt = None
+
+
+  def endDocument(self):
+    self.conn.execute('VACUUM')
+    self.conn.commit()
+    self.conn.close()
+
+  def startElement(self, name, attrs):
+    self.txt = ''
+    if name == 'entry':
+      self.sense = 0
+    elif name == 'sense':
+      self.pos = []
+      self.attr = []
+    elif name == 'gloss':
+      if attrs.has_key('xml:lang'):
+        self.lang = attrs.getValue('xml:lang')
+      else:
+        self.lang = None
+
+  def endElement(self, name):
+    self.txt = self.txt.strip()
+    if name == 'ent_seq':
+      self.cur_entry = int(self.txt)
+    elif name == 'keb':
+      self.conn.execute("INSERT INTO kanji VALUES (?,?)", (self.cur_entry, self.txt))
+    elif name == 'reb':
+      self.conn.execute("INSERT INTO reading VALUES (?,?,?)", (self.cur_entry, self.txt, kana2romaji(self.txt)))
+    elif name == 'sense':
+      self.conn.execute("INSERT INTO sense VALUES (?,?,?,?)",
+          (self.cur_entry, self.sense, ','.join(self.pos), ','.join(self.attr)))
+      self.sense += 1
+    elif name == 'pos':
+      self.pos.append(self.ent[self.txt])
+    elif name in ('field', 'dial',):
+      self.attr.append(self.ent[self.txt])
+    elif name == 'gloss':
+      if self.lang is None:
+        self.lang = 'en'
+      self.conn.execute("INSERT INTO gloss VALUES (?,?,?,?)",
+          (self.cur_entry, self.sense, self.lang, self.txt))
+
+  def characters(self, content):
+    self.txt += content
+
+
+tbl_hiragana = (
+    (u'きゃ', 'kya'), (u'きゅ', 'kyu'), (u'きょ', 'kyo'),
+    (u'しゃ', 'sha'), (u'しゅ', 'shu'), (u'しょ', 'sho'),
+    (u'ちゃ', 'cha'), (u'ちゅ', 'chu'), (u'ちょ', 'cho'),
+    (u'にゃ', 'nya'), (u'にゅ', 'nyu'), (u'にょ', 'nyo'),
+    (u'ひゃ', 'hya'), (u'ひゅ', 'hyu'), (u'ひょ', 'hyo'),
+    (u'みゃ', 'mya'), (u'みゅ', 'myu'), (u'みょ', 'myo'),
+    (u'りゃ', 'rya'), (u'りゅ', 'ryu'), (u'りょ', 'ryo'),
+    (u'ぎゃ', 'gya'), (u'ぎゅ', 'gyu'), (u'ぎょ', 'gyo'),
+    (u'じゃ', 'ja'),  (u'じゅ', 'ju'),  (u'じょ', 'jo'),
+    (u'ぢゃ', 'ja'),  (u'ぢゅ', 'ju'),  (u'ぢょ', 'jo'),
+    (u'びゃ', 'bya'), (u'びゅ', 'byu'), (u'びょ', 'byo'),
+    (u'ぴゃ', 'pya'), (u'ぴゅ', 'pyu'), (u'ぴょ', 'pyo'),
+    (u'あ', 'a'),   (u'い', 'i'),   (u'う', 'u'),   (u'え', 'e'),   (u'お', 'o'),
+    (u'か', 'ka'),  (u'き', 'ki'),  (u'く', 'ku'),  (u'け', 'ke'),  (u'こ', 'ko'),
+    (u'さ', 'sa'),  (u'し', 'shi'), (u'す', 'su'),  (u'せ', 'se'),  (u'そ', 'so'),
+    (u'た', 'ta'),  (u'ち', 'chi'), (u'つ', 'tsu'), (u'て', 'te'),  (u'と', 'to'),
+    (u'な', 'na'),  (u'に', 'ni'),  (u'ぬ', 'nu'),  (u'ね', 'ne'),  (u'の', 'no'),
+    (u'は', 'ha'),  (u'ひ', 'hi'),  (u'ふ', 'fu'),  (u'へ', 'he'),  (u'ほ', 'ho'),
+    (u'ま', 'ma'),  (u'み', 'mi'),  (u'む', 'mu'),  (u'め', 'me'),  (u'も', 'mo'),
+    (u'や', 'ya'),  (u'ゆ', 'yu'),  (u'よ', 'yo'),
+    (u'ら', 'ra'),  (u'り', 'ri'),  (u'る', 'ru'),  (u'れ', 're'),  (u'ろ', 'ro'),
+    (u'わ', 'wa'),  (u'ゐ', 'wi'),  (u'ゑ', 'we'),  (u'を', 'wo'),
+    (u'ん', 'n'),
+    (u'が', 'ga'),  (u'ぎ', 'gi'),  (u'ぐ', 'gu'),  (u'げ', 'ge'),  (u'ご', 'go'),
+    (u'ざ', 'za'),  (u'じ', 'ji'),  (u'ず', 'zu'),  (u'ぜ', 'ze'),  (u'ぞ', 'zo'),
+    (u'だ', 'da'),  (u'ぢ', 'ji'),  (u'づ', 'zu'), (u'で', 'de'),  (u'ど', 'do'),
+    (u'ば', 'ba'),  (u'び', 'bi'),  (u'ぶ', 'bu'),  (u'べ', 'be'),  (u'ぼ', 'bo'),
+    (u'ぱ', 'pa'),  (u'ぴ', 'pi'),  (u'ぷ', 'pu'),  (u'ぺ', 'pe'),  (u'ぽ', 'po'),
+    (u'ぁ', 'a'),   (u'ぃ', 'i'),   (u'ぅ', 'u'),   (u'ぇ', 'e'),   (u'ぉ', 'o'),
+    )
+
+tbl_katakana = (
+    (u'イェ', 'ye'),
+    (u'ヴァ', 'va'),  (u'ヴィ', 'vi'),  (u'ヴェ', 've'),  (u'ヴォ', 'vo'),
+    (u'ヴャ', 'vya'), (u'ヴュ', 'vyu'), (u'ヴョ', 'vyo'),
+    (u'ブュ', 'byu'),
+    (u'シェ', 'she'), (u'ジェ', 'je'),
+    (u'チェ', 'che'),
+    (u'スィ', 'si'),  (u'ズィ', 'zi'),
+    (u'ティ', 'ti'),  (u'トゥ', 'tu'),  (u'テュ', 'tyu'), (u'ドュ', 'dyu'),
+    (u'ディ', 'di'),  (u'ドゥ', 'du'),  (u'デュ', 'dyu'),
+    (u'ツァ', 'tsa'), (u'ツィ', 'tsi'), (u'ツェ', 'tse'), (u'ツォ', 'tso'),
+    (u'ファ', 'fa'),  (u'フィ', 'fi'),  (u'ホゥ', 'hu'),  (u'フェ', 'fe'),  (u'フォ', 'fo'),   (u'フュ', 'fyu'),
+    (u'ウィ', 'wi'),  (u'ウェ', 'we'),  (u'ウォ', 'wo'),
+    (u'クヮ', 'kwa'), (u'クァ', 'kwa'), (u'クィ', 'kwi'), (u'クェ', 'kwe'), (u'クォ', 'kwo'),
+    (u'グヮ', 'gwa'), (u'グァ', 'gwa'), (u'グィ', 'gwi'), (u'グェ', 'gwe'), (u'グォ', 'gwo'),
+    (u'ヴ', 'vu'),
+    ) + tuple([ (''.join(unichr(ord(c)+0x60) for c in k), v) for k,v in tbl_hiragana])
+
+tbl_symbols = (
+    (u'〜', '~'), (u'。', '.'), (u'、', ','), (u'　', ' '),
+    )
+
+def kana2romaji(txt):
+  txt = unicode(txt)
+  for k,v in tbl_hiragana + tbl_katakana + tbl_symbols:
+    txt = txt.replace(k, v)
+  txt = re.sub(ur'[っッ]([bcdfghjkmnprstvwz])', r'\1\1', txt)
+  txt = re.sub(ur'([aeiou])ー', r'\1\1', txt)
+  txt = re.sub(ur'[・ー−―]', '-', txt)
+  txt = re.sub(ur'[っッ]', r'-tsu', txt)
+  txt = re.sub(ur'[\uff00-\uff5e]', lambda m: unichr(ord(m.group(0))-0xfee0), txt)
+  try:
+    txt = str(txt)
+  except UnicodeEncodeError, e:
+    print >>sys.stderr, 'Warning: characters not translated in "%s"'% repr(txt)
+    txt = txt.encode('ascii', 'replace')
+  return txt
+
+
+
+if __name__ == '__main__':
+  from optparse import OptionParser
+  import gzip, urllib, StringIO
+
+  parser = OptionParser(
+      usage="%prog [OPTIONS] [JMDICT-XML]",
+      description="""\
+Build jpy database from XML JMdict.
+JMdict can be provided as a file or downloaded (~5Mo).
+If filename ends with '.gz', it is automatically gunzipped.
+""")
+  parser.add_option('-d', '--download', dest='download', action='store_true',
+      help="download lastest JMdict (take some time)",
+      )
+  parser.add_option('-o', '--output', dest='output',
+      help="output db file (default 'jpy.db')",
+      metavar='DB'
+      )
+  parser.set_defaults(download=False, output='jpy.db')
+
+  (opts, args) = parser.parse_args()
+  
+  if opts.download:
+    if len(args) != 0:
+      parser.error("invalid parameter count")
+    print "downloading JMdict..."
+    f = StringIO.StringIO( urllib.urlopen(jmdict_url).read() )
+    f = gzip.GzipFile(fileobj=f)
+  else:
+    if len(args) != 1:
+      parser.error("invalid parameter count")
+    f = args.pop(0)
+    if f.endswith('.gz'):
+      f = gzip.GzipFile(f)
+
+  parser = xml.sax.make_parser()
+  parser.setContentHandler(JMDictHandler(opts.output))
+  parser.parse(f)
+
+
---
+++ jpy.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python
+# vim: fileencoding=utf-8
+
+import sqlite3
+import os, re
+
+import pygtk
+pygtk.require('2.0')
+import gtk, pango
+
+
+class JpyApp:
+
+  def __init__(self, db=None):
+    self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+    self.window.set_resizable(True)
+    self.window.set_size_request(350,350)
+    self.window.set_title('jpy')
+    self.window.connect('delete_event', lambda w,e: gtk.main_quit())
+
+    box = gtk.VBox(False, 0)
+    self.window.add(box)
+
+    self.w_search = gtk.combo_box_entry_new_text()
+    self.w_search.child.connect('activate', lambda w: self.display_results(w.get_text()))
+    self.w_search.child.modify_text(gtk.STATE_NORMAL, gtk.gdk.color_parse('black'))
+    self.w_search.child.modify_font(pango.FontDescription('sans 12'))
+    box.pack_start(self.w_search, expand=False, fill=False)
+
+    # Tags (customize style of displayed text here)
+    tagtable = gtk.TextTagTable()
+    tag = gtk.TextTag('jap')
+    tag.set_property('foreground', 'darkblue')
+    tag.set_property('size-points', 14)
+    tag.set_property('pixels-above-lines', 8)
+    #tag.set_property('weight', pango.WEIGHT_BOLD)
+    tagtable.add(tag)
+    tag = gtk.TextTag('pos')
+    tag.set_property('foreground', 'darkred')
+    tagtable.add(tag)
+    tag = gtk.TextTag('attr')
+    tag.set_property('foreground', 'purple')
+    tagtable.add(tag)
+    tag = gtk.TextTag('sense-num')
+    tag.set_property('foreground', 'darkgreen')
+    tagtable.add(tag)
+  
+    self.w_result = gtk.TextView(gtk.TextBuffer(tagtable))
+    self.w_result.set_editable(False) 
+    self.w_result.set_cursor_visible(False) 
+    self.w_result.set_wrap_mode(gtk.WRAP_WORD)
+    self.w_result.set_justification(gtk.JUSTIFY_LEFT)
+    self.w_result.modify_text(gtk.STATE_NORMAL, gtk.gdk.color_parse('black'))
+    self.w_result.modify_font(pango.FontDescription('sans 12'))
+
+    self.w_display = gtk.ScrolledWindow()
+    self.w_display.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+    self.w_display.add_with_viewport(self.w_result)
+    box.pack_start(self.w_display, expand=True, fill=True)
+
+    box.show()
+    self.w_search.show()
+    self.w_result.show()
+    self.w_display.show()
+    self.window.show()
+
+    # Connect to SQLite database
+    if db is None:
+      db = os.path.join(os.path.dirname(__file__),'jpy.db')
+    Query.connect(db)
+
+
+  def main(self):
+    gtk.main()
+
+
+  def display_results(self, txt):
+    buffer = self.w_result.get_buffer()
+    buffer.set_text('')
+    if len(txt) == 0:
+      return
+
+    result = Query(txt, limit=50).execute()
+
+    self.w_search.child.select_region(0,-1)
+    self.w_search.prepend_text(txt)
+    self.w_search.remove_text(10)
+    iter = buffer.get_end_iter()
+
+    # Format results (customize display format here)
+    for e in result:
+      s = ', '.join(e.reb)
+      if len(e.keb):
+        s = ', '.join(e.keb) + ' / ' + s
+      buffer.insert_with_tags_by_name(iter, s+"\n", 'jap')
+      for i,s in enumerate(e.sense):
+        buffer.insert_with_tags_by_name(iter, "%d) "%(i+1), 'sense-num')
+        if len(s[0]) > 0:
+          buffer.insert_with_tags_by_name(iter, ' '.join(s[0])+'  ', 'pos')
+        if len(s[1]) > 0:
+          buffer.insert_with_tags_by_name(iter, '['+' '.join(s[1])+'] ', 'attr')
+        buffer.insert(iter, ', '.join(s[2])+'\n')
+
+    # Scroll at the top
+    self.w_display.get_vadjustment().set_value(0)
+
+
+class Query():
+  """Search query and database connection.
+
+  Instance attributes:
+    to_jp -- search for Japanese translation (default: False)
+    pattern -- search pattern, with (converted) wildcards
+    limit -- maximum number of results (no limit: negative number, the default)
+
+  Instance methods:
+    build -- build a query from an ordinary search string
+    execute -- execute the query and return the result Entry list
+
+  Class method:
+    connect -- initialize the SQLite DB connection
+  
+  Class attribute:
+    conn -- SQLite Connection object
+
+  Instance attribute:
+
+  """
+
+  conn = None
+  
+  def __init__(self, s=None, to_jp=False, limit=None):
+    """Build a query.
+    Arguments values may be overwritten by special tags in search string.
+
+    """
+
+    self.to_jp = to_jp
+    self.limit = limit
+    self.pattern = None
+    if s is not None:
+      self.build(s)
+
+  def build(self, s):
+    """Build a query from an ordinary search string.
+
+    If there is no special character, 'pat' is equivalent to 'pat%'.
+
+    Special characters:
+      *, % : 0 or more characters
+      ?, _ : single character
+      / as first character : translate to Japanese
+    
+    """
+  
+    if len(s) == 0:
+      return ''
+
+    s = unicode(s)
+    if s[0] == '/':
+      self.to_jp = True
+      s = s[1:]
+    s = re.sub(ur'[*＊％]', '%', s)
+    s = re.sub(ur'[?？＿]', '_', s)
+    if re.search(r'[_%]', s) is None:
+      self.pattern = s + '%'
+    else:
+      self.pattern = s
+
+  def execute(self):
+    """Execute the query and return the result Entry list."""
+
+    cursor = self.conn.cursor()
+    limit = self.limit
+    if limit is None:
+      limit = -1
+
+    # Get ent_id to display
+    if self.to_jp:
+      # To Japanese
+      tables, fields = ('gloss',), ('gloss',)
+    elif re.match('^[ -~]*$', self.pattern):
+      # ASCII only: romaji
+      tables, fields = ('reading',), ('romaji',)
+    else:
+      # Unicode: first kanji, then kana
+      tables, fields = ('kanji','reading',), ('keb','reb',)
+    # Fetch results
+    query = "SELECT DISTINCT ent_id FROM %s WHERE %s LIKE ? ORDER BY length(%s) LIMIT ?"
+    for t,f in zip(tables,fields):
+      cursor.execute(query % (t, f, f), (self.pattern,limit))
+      ent_id = tuple( x[0] for x in cursor )
+      if len(ent_id) != 0:
+        break
+
+    ent_id_list = '('+','.join(str(i) for i in ent_id)+')'
+
+    # Note: a dictionary is not sorted
+    # Entry order is still obtained from ent_id.
+    result = {}
+    for e in ent_id:
+      result[e] = Entry(e)
+
+    cursor.execute("SELECT ent_id, keb FROM kanji WHERE ent_id IN "+ent_id_list)
+    for s in cursor:
+      result[s[0]].keb.append(s[1])
+    cursor.execute("SELECT ent_id, reb FROM reading WHERE ent_id IN "+ent_id_list)
+    for s in cursor:
+      result[s[0]].reb.append(s[1])
+    cursor.execute("SELECT ent_id, sense_num, pos, attr FROM sense WHERE ent_id IN "+ent_id_list+" ORDER BY ent_id, sense_num")
+    for s in cursor:
+      result[s[0]].sense.append( (filter(None, s[2].split(',')), filter(None, s[3].split(',')), []) )
+    cursor.execute("SELECT ent_id, sense_num, gloss FROM gloss WHERE ent_id IN "+ent_id_list)
+    for s in cursor:
+      result[s[0]].sense[s[1]-1][2].append( s[2] )
+
+    return [ result[e] for e in ent_id ]
+
+  @classmethod
+  def connect(cls, dbfile):
+    cls.conn = sqlite3.connect(dbfile)
+
+
+class Entry:
+  """Dictionary entry.
+
+  Attributes:
+    seq -- entry seq ID
+    keb -- kanji writings
+    reb -- kana writings
+    sense -- definition list (pos list, attr. list, gloss list)
+  
+  """
+
+  def __init__(self, seq):
+    self.seq = seq
+    self.keb, self.reb, self.sense = [], [], []
+
+
+
+if __name__ == '__main__':
+  from optparse import OptionParser
+
+  parser = OptionParser(
+      description="PyGTK interface for JMdict.",
+      usage="%prog [OPTIONS] [SEARCH]"
+      )
+  parser.add_option('-d', dest='db', metavar='FILE',
+      help="SQLite database to use")
+  (opts, args) = parser.parse_args()
+
+  app = JpyApp(opts.db)
+  if len(args) > 0:
+    app.w_search.child.set_text(args[0])
+    app.w_search.child.activate()
+  app.main()
+
